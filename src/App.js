ead8d2f84898b337d4c18cc6e199b6fe3740481a
Juan Andres <sjuanandres94@gmail.com>
2021-09-23 12:37:38 -0500

    Integración de React JS y su configuración base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './styles/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+export default App;
