d5fa096e85ed0b8ae06a6498759529bd9b6c63da
Juan Andres <sjuanandres94@gmail.com>
2021-09-29 21:06:36 -0500

    Ajuste de Navbar para regresar con icono

--- src/components/InternNavbar.jsx
+++ src/components/InternNavbar.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 const InternNavbar = () => {
     return (
         <div>
-            <Link to= "/menu">
-                <ul className="Navbar flex bg-blue-500  justify-between h-16">
+            <ul className="Navbar flex bg-blue-500  justify-between h-16">
+                <Link to= "/menu">
                     <img className='h-12 w-50 m-3' src={LogoHome} alt='Home' />
-                    <li className="m-2">Perfil</li>
-                </ul>
-            </Link>
+                </Link>
+                <li className="m-2">Perfil</li>
+            </ul>
         </div>    
     )
 }
