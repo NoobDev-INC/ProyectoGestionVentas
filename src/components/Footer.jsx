387b94be1042124e0ea9ef632cf6eaf745ce9116
Felipe <andresfppy@gmail.com>
2021-09-29 20:59:55 -0500

    Footer Realizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer>
-            Este es el Footer
-        </footer>
+        <footer class="text-center bg-blue-500 text-white bg-teal-900 py-4">
+        <ul class="md:flex justify-between">
+            <li><a href="http://">Privacidad </a></li>
+            <li><a href="http://">Términos y condiciones</a></li>
+            <li>&#174; 2021 NoobDev</li>
+        </ul>
+    </footer>
     )
 }
 
