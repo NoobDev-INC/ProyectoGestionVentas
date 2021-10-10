81da53752d5b784d263866deefffb23e05be0952
Jose David Salazar <jdsaja98@gmail.com>
2021-10-10 14:11:45 -0500

    productos actualizado

--- src/pages/product/Productos.jsx
+++ src/pages/product/Productos.jsx
@@ -1,42 +1,35 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+import { nanoid } from 'nanoid';
+import { Dialog, Tooltip } from '@material-ui/core';
+import { obtenerProductos } from 'utils/api';
 import 'react-toastify/dist/ReactToastify.css';
-import { Link } from 'react-router-dom';
-const productosBackend = [
-  {
-    nombre: 'Galaxy S10',
-    marca: 'Samsung',
-    ID: 165,
-    precio: 3500000,
-  },
-  {
-    nombre: 'Iphone 11',
-    marca: 'Apple',
-    ID: 136,
-    precio: 4500000,
-  },
-  {
-    nombre: 'Redmi note 9',
-    marca: 'Xiaomi',
-    ID: 204,
-    precio: 950000
-  },
-];
 
 const Productos = () => {
   const [mostrarTabla, setMostrarTabla] = useState(true);
   const [productos, setProductos] = useState([]);
-  const [textoBoton, setTextoBoton] = useState('Añadir nuevo producto');
+  const [textoBoton, setTextoBoton] = useState('Crear Nuevo Producto');
   const [colorBoton, setColorBoton] = useState('blue');
+  const [ejecutarConsulta, setEjecutarConsulta] = useState(true);
 
   useEffect(() => {
-    //obtener lista de productos desde el backend
-    setProductos(productosBackend);
-  }, []);
+    console.log('consulta', ejecutarConsulta);
+    if (ejecutarConsulta) {
+      obtenerProductos(setProductos, setEjecutarConsulta);
+    }
+  }, [ejecutarConsulta]);
+
+  useEffect(() => {
+  
+    if (mostrarTabla) {
+      setEjecutarConsulta(true);
+    }
+  }, [mostrarTabla]);
 
   useEffect(() => {
     if (mostrarTabla) {
-      setTextoBoton('Añadir nuevo producto');
+      setTextoBoton('Crear Nuevo Producto');
       setColorBoton('blue');
     } else {
       setTextoBoton('Mostrar Todos los Productos');
@@ -45,91 +38,291 @@ const Productos = () => {
   }, [mostrarTabla]);
   return (
     <div className='flex h-full w-full flex-col items-center justify-start p-8'>
-      <div className='flex flex-col'>
+      <div className='flex flex-col w-full'>
         <h2 className='text-3xl font-extrabold text-gray-900'>
-          Página de administración de Productos
+          Página de administración de productos
         </h2>
         <button
           onClick={() => {
             setMostrarTabla(!mostrarTabla);
           }}
-          className={`text-white bg-${colorBoton}-500 border  border-gray-600 p-2 rounded-xl m-2`}
+          className={`text-white bg-${colorBoton}-500 hover:bg-${colorBoton}-700 border  border-gray-600 p-2 rounded-xl m-2`}
         >
           {textoBoton}
         </button>
       </div>
       {mostrarTabla ? (
-        <TablaProductos listaProductos={productos} />
+        <TablaProductos listaProductos={productos} setEjecutarConsulta={setEjecutarConsulta} />
       ) : (
         <FormularioCreacionProductos
           setMostrarTabla={setMostrarTabla}
-          listaProductos={productos}
+          listaProductos={vehiculos}
           setProductos={setProductos}
         />
       )}
       <ToastContainer position='bottom-center' autoClose={5000} />
-      <Link to='/BuscarP'>
-        <button
-         class='text-white bg-blue-500 border border-gray-600 p-2 rounded-xl m-2'
-        >
-          Buscar producto
-        </button> 
-      </Link>
     </div>
   );
 };
 
-const TablaProductos = ({ listaProductos }) => {
+const TablaProductos = ({ listaProductos, setEjecutarConsulta }) => {
+  const [busqueda, setBusqueda] = useState('');
+  const [productosFiltrados, setProductosFiltrados] = useState(listaProductos);
+
   useEffect(() => {
-    console.log('este es el listado de productos en el componente de tabla', listaProductos);
-  }, [listaProductos]);
+    setProductosFiltrados(
+      listaProductos.filter((elemento) => {
+        return JSON.stringify(elemento).toLowerCase().includes(busqueda.toLowerCase());
+      })
+    );
+  }, [busqueda, listaProductos]);
+
   return (
-    <div className='flex flex-col items-center justify-center'>
+    <div className='flex flex-col items-center justify-center w-full'>
+      <input
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+        placeholder='Buscar'
+        className='border-2 border-gray-700 px-3 py-1 self-start rounded-md focus:outline-none focus:border-blue-500'
+      />
       <h2 className='text-2xl font-extrabold text-gray-800'>Todos los productos</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Nombre del producto</th>
-            <th>Marca del producto</th>
-            <th>ID del producto</th>
-            <th>precio del producto</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listaProductos.map((producto) => {
-            return (
-              <tr>
-                <td>{producto.nombre}</td>
-                <td>{producto.marca}</td>
-                <td>{producto.ID}</td>
-                <td>{producto.precio}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className='hidden md:flex w-full'>
+        <table className='tabla'>
+          <thead>
+            <tr>
+              <th>Nombre del productos</th>
+              <th>Marca del producto</th>
+              <th>ID del producto</th>
+              <th>precio del producto</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {productosFiltrados.map((producto) => {
+              return (
+                <FilaProducto
+                  key={nanoid()}
+                  producto={producto}
+                  setEjecutarConsulta={setEjecutarConsulta}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className='flex flex-col w-full m-2 md:hidden'>
+        {productosFiltrados.map((el) => {
+          return (
+            <div className='bg-gray-400 m-2 shadow-xl flex flex-col p-2 rounded-xl'>
+              <span>{el.name}</span>
+              <span>{el.brand}</span>
+              <span>{el.id}</span>
+              <span>{el.price}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
+  );
+};
+
+const FilaProducto = ({ producto, setEjecutarConsulta }) => {
+  const [edit, setEdit] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [infoNuevoProducto, setInfoNuevoProducto] = useState({
+    name: producto.name,
+    brand: producto.brand,
+    id: producto.id,
+    price: producto.price,
+  });
+
+  const actualizarProducto = async () => {
+    
+    const options = {
+      method: 'PATCH',
+      url: 'https://vast-waters-45728.herokuapp.com/vehicle/update/',
+      headers: { 'Content-Type': 'application/json' },
+      data: { ...infoNuevoProducto, id: producto._id },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        toast.success('Producto modificado con éxito');
+        setEdit(false);
+        setEjecutarConsulta(true);
+      })
+      .catch(function (error) {
+        toast.error('Error modificando el producto');
+        console.error(error);
+      });
+  };
+
+  const eliminarProducto = async () => {
+    const options = {
+      method: 'DELETE',
+      url: 'https://vast-waters-45728.herokuapp.com/vehicle/delete/',
+      headers: { 'Content-Type': 'application/json' },
+      data: { id: producto._id },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        toast.success('producto eliminado con éxito');
+        setEjecutarConsulta(true);
+      })
+      .catch(function (error) {
+        console.error(error);
+        toast.error('Error eliminando el producto');
+      });
+    setOpenDialog(false);
+  };
+
+  return (
+    <tr>
+      {edit ? (
+        <>
+          <td>
+            <input
+              className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+              type='text'
+              value={infoNuevoProducto.name}
+              onChange={(e) => setInfoNuevoProducto({ ...infoNuevoProducto, name: e.target.value })}
+            />
+          </td>
+          <td>
+            <input
+              className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+              type='text'
+              value={infoNuevoProducto.brand}
+              onChange={(e) =>
+                setInfoNuevoProducto({ ...infoNuevoProducto, brand: e.target.value })
+              }
+            />
+          </td>
+          <td>
+            <input
+              className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+              type='text'
+              value={infoNuevoProducto.id}
+              onChange={(e) =>
+                setInfoNuevoProducto({ ...infoNuevoProducto, model: e.target.value })
+              }
+            />
+          </td>
+          <td>
+            <input
+              className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+              type='text'
+              value={infoNuevoProducto.price}
+              onChange={(e) =>
+                setInfoNuevoProducto({ ...infoNuevoProducto, model: e.target.value })
+              }
+            />
+          </td>
+        </>
+      ) : (
+        <>
+          <td>{producto.name}</td>
+          <td>{producto.brand}</td>
+          <td>{producto.id}</td>
+          <td>{producto.price}</td>
+        </>
+      )}
+      <td>
+        <div className='flex w-full justify-around'>
+          {edit ? (
+            <>
+              <Tooltip title='Confirmar Edición' arrow>
+                <i
+                  onClick={() => actualizarProducto()}
+                  className='fas fa-check text-green-700 hover:text-green-500'
+                />
+              </Tooltip>
+              <Tooltip title='Cancelar edición' arrow>
+                <i
+                  onClick={() => setEdit(!edit)}
+                  className='fas fa-ban text-yellow-700 hover:text-yellow-500'
+                />
+              </Tooltip>
+            </>
+          ) : (
+            <>
+              <Tooltip title='Editar Producto' arrow>
+                <i
+                  onClick={() => setEdit(!edit)}
+                  className='fas fa-pencil-alt text-yellow-700 hover:text-yellow-500'
+                />
+              </Tooltip>
+              <Tooltip title='Eliminar Vehículo' arrow>
+                <i
+                  onClick={() => setOpenDialog(true)}
+                  className='fas fa-trash text-red-700 hover:text-red-500'
+                />
+              </Tooltip>
+            </>
+          )}
+        </div>
+        <Dialog open={openDialog}>
+          <div className='p-8 flex flex-col'>
+            <h1 className='text-gray-900 text-2xl font-bold'>
+              ¿Está seguro de querer eliminar el producto?
+            </h1>
+            <div className='flex w-full items-center justify-center my-4'>
+              <button
+                onClick={() => eliminarProducto()}
+                className='mx-2 px-4 py-2 bg-green-500 text-white hover:bg-green-700 rounded-md shadow-md'
+              >
+                Sí
+              </button>
+              <button
+                onClick={() => setOpenDialog(false)}
+                className='mx-2 px-4 py-2 bg-red-500 text-white hover:bg-red-700 rounded-md shadow-md'
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </Dialog>
+      </td>
+    </tr>
   );
 };
 
 const FormularioCreacionProductos = ({ setMostrarTabla, listaProductos, setProductos }) => {
   const form = useRef(null);
 
-  const submitForm = (e) => {
+  const submitForm = async (e) => {
     e.preventDefault();
     const fd = new FormData(form.current);
-    
+
     const nuevoProducto = {};
     fd.forEach((value, key) => {
       nuevoProducto[key] = value;
     });
 
+    const options = {
+      method: 'POST',
+      url: 'http://localhost:5000/productos/nuevo',
+      headers: { 'Content-Type': 'application/json' },
+      data: { name: nuevoProducto.name, brand: nuevoProducto.brand, id: nuevoProducto.id, price: nuevoProducto.brand },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        toast.success('Producto agregado con éxito');
+      })
+      .catch(function (error) {
+        console.error(error);
+        toast.error('Error creando un producto');
+      });
+
     setMostrarTabla(true);
-    setProductos([...listaProductos, nuevoProducto]);
-    // identificar el caso de éxito y mostrar un toast de éxito
-    toast.success('Producto agregado con éxito');
-    // identificar el caso de error y mostrar un toast de error
-    // toast.error('Error creando un vehículo');
   };
 
   return (
@@ -139,52 +332,44 @@ const FormularioCreacionProductos = ({ setMostrarTabla, listaProductos, setProdu
         <label className='flex flex-col' htmlFor='nombre'>
           Nombre del producto
           <input
-            name='nombre'
+            name='name'
             className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
             type='text'
-            placeholder='nombre telefono'
+            placeholder='Galaxy S10'
             required
           />
         </label>
         <label className='flex flex-col' htmlFor='marca'>
-          Marca del producto
-          <select
+          Nombre del producto
+          <input
+            name='brand'
             className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
-            name='marca'
+            type='text'
+            placeholder='Samsung'
             required
-            defaultValue={0}
-          >
-            <option disabled value={0}>
-              Seleccione una opción
-            </option>
-            <option>Apple</option>
-            <option>Samsung</option>
-            <option>Xiaomi</option>
-            <option>Huawei</option>
-            <option>Nokia</option>
-          </select>
+          />
         </label>
-        <label className='flex flex-col' htmlFor='ID'>
+        <label className='flex flex-col' htmlFor='id'>
           ID del producto
           <input
-            name='ID'
+            name='id'
             className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
             type='number'
-            min={0}
-            max={1000}
-            placeholder='0'
+            min={1}
+            max={99999}
+            placeholder='1246'
             required
           />
         </label>
         <label className='flex flex-col' htmlFor='precio'>
-          precio del producto
+          Precio del producto
           <input
             name='precio'
             className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
             type='number'
-            min={0}
+            min={500000}
             max={20000000}
-            placeholder='0'
+            placeholder='3300000'
             required
           />
         </label>
@@ -193,10 +378,9 @@ const FormularioCreacionProductos = ({ setMostrarTabla, listaProductos, setProdu
           type='submit'
           className='col-span-2 bg-green-400 p-2 rounded-full shadow-md hover:bg-green-600 text-white'
         >
-          Guardar Producto
+          Guardar producto
         </button>
       </form>
-
     </div>
   );
 };
