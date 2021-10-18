298bbfbcbc6934fad39e9b431382ec289e1238a2
ValenOso <90661421+ValenOso@users.noreply.github.com>
2021-10-17 22:44:41 -0500

    actualizado PrivateRoute

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
 
 const PrivateRoute=({children})=>{
-    const {user,isAuthenticated, isloading}=useAuth0();
+    const {user,isAuthenticated,isloading}=useAuth0();
     
     if (isloading) return <div>loading...</div>;
 
-    if (!isAuthenticated){
-        return loginWithdiRedirect();
-    }
-    return <>{children}</>;
+    return isAuthenticated ?(
+        <>{children}</>
+    ):(
+        <div className='text-9xl text-red-500'>No estas autorizado para ver este sitio</div>
+        
+    )
 };
 export default PrivateRoute;
