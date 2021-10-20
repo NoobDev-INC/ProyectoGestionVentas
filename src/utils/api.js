3b96d9f5fa325414814472c693c98b6e244a2658
Felipe <andresfppy@gmail.com>
2021-10-20 00:28:27 -0500

    cambiar url api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 import { MdGeneratingTokens } from 'react-icons/md';
 
+const baseURL = "https://frozen-beach-86139.herokuapp.com/"
+
 export const obtenerProductos = async (successCallback, errorCallback) => {
-  const options = { method: 'GET', url: 'http://localhost:5000/productos/' };
+  const options = { method: 'GET', url: `${baseURL}/productos/` };
   await axios.request(options).then(successCallback).catch(errorCallback);
 };
 
 export const crearProducto = async (data, successCallback, errorCallback) => {
   const options = {
     method: 'POST',
-    url: 'http://localhost:5000/productos/',
+    url: `${baseURL}/productos/`,
     headers: { 'Content-Type': 'application/json' },
     data,
   };
@@ -19,7 +21,7 @@ export const crearProducto = async (data, successCallback, errorCallback) => {
 export const editarProducto = async (id, data, successCallback, errorCallback) => {
   const options = {
     method: 'PATCH',
-    url: `http://localhost:5000/productos/${id}/`,
+    url: `${baseURL}/productos/${id}/`,
     headers: { 'Content-Type': 'application/json' },
     data,
   };
@@ -29,7 +31,7 @@ export const editarProducto = async (id, data, successCallback, errorCallback) =
 export const eliminarProducto = async (id, successCallback, errorCallback) => {
   const options = {
     method: 'DELETE',
-    url: `http://localhost:5000/productos/${id}/`,
+    url: `${baseURL}/productos/${id}/`,
     headers: { 'Content-Type': 'application/json' },
   };
   await axios.request(options).then(successCallback).catch(errorCallback);
@@ -38,7 +40,7 @@ export const eliminarProducto = async (id, successCallback, errorCallback) => {
 export const otenerDatosUsuario = async (successCallback, errorCallback) => {
   const options = {
     method: 'GET',
-    url: 'http://localhost:5000/usuarios/self',
+    url: `${baseURL}/usuarios/self/`,
     headers: { 
     },
   };
